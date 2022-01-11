8fc671d23eb5982120b39d607aa17caca58ab44b
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2022-01-11 11:20:00 +0100

    feat admin

--- admin/handlers.go
+++ admin/handlers.go
@@ -1,20 +1,24 @@
 package admin
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/orpheeh/jalbv-backend/account"
+	"github.com/golang-jwt/jwt"
 )
 
 func getAuthAdmin(c *gin.Context) {
 	value, exist := c.Get("account")
 	if exist {
-		response, err := getAdminByAccount(value.(account.Account).ID)
+		claims := value.(jwt.MapClaims)
+		fmt.Println(claims)
+		id := claims["ID"].(string)
+		response, err := getAdminByAccount(id)
 		if err != nil {
 			c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "User account not found"})
 		} else {
-			c.IndentedJSON(http.StatusUnauthorized, response)
+			c.IndentedJSON(http.StatusOK, response)
 		}
 	} else {
 		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "User account not found"})
